662e18f2512f514e2dc4f4e3822c534ce69c39c4
unknown <Jefferson-chidiox>
2024-06-21 10:04:04 +0100

    first commit

---
+++ assets/js/blog.js
@@ -0,0 +1,163 @@
+const selectElement = selector => {
+    const element = document.querySelector(selector)
+    if(element) return element;
+    throw new Error(`ERROR!! ERROR!! make sure that ${selector} exists or is 
+    typed correctly.`);
+};
+
+const scrollHeader = () => {
+    const headerElement = selectElement('#header');
+    if(this.scrollY >= 15){
+        headerElement.classList.add('activated');
+    }else{
+        headerElement.classList.remove('activated');
+    }
+};
+window.addEventListener('scroll', scrollHeader);
+
+const menuToggleIcon = selectElement('#menu-toggle-icon');
+const toggleMenu = () => {
+    const mobileMenu = selectElement('#menu');
+    mobileMenu.classList.toggle('activated');
+    menuToggleIcon.classList.toggle('activated');
+
+};
+menuToggleIcon.addEventListener('click', toggleMenu);
+
+//open and close search form popup
+const formOpenBtn = selectElement ('#search-icon');
+const formCloseBtn = selectElement ('#form-close-btn');
+const searchFormContainer = selectElement ('#search-form-container');
+
+//  close the search form popup on esc keypad
+formOpenBtn.addEventListener('click', () => searchFormContainer.classList.add
+('activated'));
+
+formCloseBtn.addEventListener('click', () => searchFormContainer.classList.remove
+('activated'));
+
+window.addEventListener('keyup', event =>{
+    if(event.key === 'Escape') searchFormContainer.classList.remove
+    ('activated');
+})
+//switch theme/add to local storage
+const bodyElement = document.body;
+const themeToggleBtn = selectElement('#theme-toggle-btn');
+const currentTheme = localStorage.getItem('currentTheme');
+
+if(currentTheme){
+    bodyElement.classList.add('light-theme');
+}
+
+themeToggleBtn.addEventListener('click', () =>{
+    bodyElement.classList.toggle('light-theme')
+    if(bodyElement.classList.contains('light-theme')){
+        localStorage.setItem('currentTheme', 'themeActive');
+    }else{
+        localStorage.removeItem('currentTheme');
+    }
+});
+//swiper
+const swiper = new Swiper('.swiper',{
+    slidesPerView: 1,
+    spaceBetween:20,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    breakpoints: {
+        700: {
+            slidesPerView: 2
+        },
+        1200: {
+            slidesPerView: 3
+        }
+    }
+});
+
+// scroll to top button
+// Set a variable for our button element.
+const scrollToTopButton = document.getElementById('js-top');
+
+// Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
+const scrollFunc = () => {
+  // Get the current scroll value
+  let y = window.scrollY;
+  
+  // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
+  if (y > 0) {
+    scrollToTopButton.className = "top-link show";
+  } else {
+    scrollToTopButton.className = "top-link hide";
+  }
+};
+
+window.addEventListener("scroll", scrollFunc);
+
+const scrollToTop = () => {
+  // Let's set a variable for the number of pixels we are from the top of the document.
+  const c = document.documentElement.scrollTop || document.body.scrollTop;
+  
+  // If that number is greater than 0, we'll scroll back to 0, or the top of the document.
+  // We'll also animate that scroll with requestAnimationFrame:
+  // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    // ScrollTo takes an x and a y coordinate.
+    // Increase the '10' value to get a smoother/slower scroll!
+    window.scrollTo(0, c - c / 10);
+  }
+};
+
+// When the button is clicked, run our ScrolltoTop function above!
+scrollToTopButton.onclick = function(e) {
+  e.preventDefault();
+  scrollToTop();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const selector = '.nav__link';
+    const elems = Array.from( document.querySelectorAll( selector ) );
+    const navigation = document.querySelector( 'nav' );
+    
+    function makeActive( evt ) {
+      const target = evt.target;
+      
+      if ( !target || !target.matches( selector ) ) {
+        return;
+      }
+      
+      elems.forEach( elem => elem.classList.remove( 'active' ) );
+        
+        evt.target.classList.add( 'active' );
+    };
+    
+    navigation.addEventListener( 'mousedown', makeActive );
+    
+    } );
+    
+   
+//nav-active//
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav .container ul li");
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+});
